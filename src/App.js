fa8dd0e4ad5f83d55245819afdd1dbc4c39df13b
Luigi Morel <morelmiles@gmail.com>
2021-10-30 13:58:33 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mr-4">
+      <p>hello from the main component</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/FindJobs.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FindJobs = () => {
+  return (
+    <div>
+      <p>Hello from the find jobs component</p>
+    </div>
+  );
+};
