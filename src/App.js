b9f74f64e2c8d73ad9dd80f5579b05e443f9f4ed
Luigi Morel <morelmiles@gmail.com>
2021-11-02 11:45:25 +0300

    Add main app routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+//Components
 import FindCandidates from "./pages/FindCandidates";
 import FindJobs from "./pages/FindJobs";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
       <Switch>
+        <Route exact path="/" component={LandingPage} />
         <Route exact path="/candidates" component={FindJobs} />
-        <Route exact path="/employer" component={FindCandidates} />
+        <Route exact path="/employers" component={FindCandidates} />
       </Switch>
     </>
   );
