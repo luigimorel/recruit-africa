d4ee3abdda4cda22dab385857c4bc7ffd943c641
Luigi Morel <morelmiles@gmail.com>
2021-11-09 05:19:00 +0300

    Add route for candidate sign up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import SignUpCandidates from "./pages/accounts/candidates/signUp.candidates";
 
 //Components
 import FindCandidates from "./pages/FindCandidates";
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/candidates" component={FindJobs} />
           <Route exact path="/employers" component={FindCandidates} />
+          <Route exact path="/candidate-signup" component={SignUpCandidates} />
         </Switch>
       </BrowserRouter>
     </>
