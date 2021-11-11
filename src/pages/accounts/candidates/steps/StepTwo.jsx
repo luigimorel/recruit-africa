cc1967ca2b1d8669bc8badb4c2992db45d4286ba
Luigi Morel <morelmiles@gmail.com>
2021-11-11 10:17:11 +0300

    Add step two

---
+++ src/pages/accounts/candidates/steps/StepTwo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const StepTwo = () => {
+  return (
+    <>
+      <h3 className=" font-bold text-2xl mb-12">Brief Bio </h3>
+      <div className="flex flex-row justify-start mb-2.5">
+        <p className="mr-14 text-gray-800">Describe what you learnt</p>
+        <p className="text-gray-800">Minimum 70 characters</p>
+      </div>
+
+      <textarea
+        name=""
+        id=""
+        cols="50"
+        placeholder="I’m motivated and a team player"
+        className="pt-2 px-3"
+        rows="10"
+        minLength="70"
+      ></textarea>
+    </>
+  );
+};
+
+export default StepTwo;
