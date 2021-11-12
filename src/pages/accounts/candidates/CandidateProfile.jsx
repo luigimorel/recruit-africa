8c3a39a1ecd1309f1a58d35e38a6fc1903d1ebbf
Luigi Morel <morelmiles@gmail.com>
2021-11-12 10:19:38 +0300

    Remove unused variables6

--- src/pages/accounts/candidates/CandidateProfile.jsx
+++ src/pages/accounts/candidates/CandidateProfile.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Assets
 import { ReactComponent as Logo } from "./../../../assets/Logo.svg";
 import { ReactComponent as ArrowLeft } from "./../../../assets/accounts/ArrowLeft.svg";
 
 //Components
-import StepOne from "./steps/StepOne";
-import StepTwo from "./steps/StepTwo";
-import StepThree from "./steps/StepThree";
-import StepFour from "./steps/StepFour";
+import StepFive from "./steps/StepFive";
 
 const CandidateProfile = () => {
   return (
@@ -16,8 +14,9 @@ const CandidateProfile = () => {
       <div className=" bg-gray-800 fixed">
         <div className="  bg-candidates-gb h-screen w-80  ">
           <div className="ml-10  mt-10">
-            <Logo className="mb-80" />
-
+            <Link to="/">
+              <Logo className="mb-80" />
+            </Link>
             <div className="text-white ">
               <h3 className="font-bold text-2xl mb-2 ">Build your resume</h3>
               <p className="mr-4 font-medium text-lg ">
@@ -29,14 +28,14 @@ const CandidateProfile = () => {
         </div>
       </div>
 
-      <div className=" ml-28 mr-36  w-full relative pl-80 h-full">
+      <div className=" ml-28 mr-36  w-full relative pl-80 min-h-full">
         <div className=" mt-16">
           <div className=" h-3  bg-gray-300 mb-11">
             {/* Add progress bar here */}
           </div>
 
           <form action="" method="post">
-            <StepFour />
+            <StepFive />
           </form>
           <div className="flex flex-row mt-9 pb-10">
             <button className=" bg-yellow-500 uppercase mt-4 py-4 px-24 font-bold text-sm">
