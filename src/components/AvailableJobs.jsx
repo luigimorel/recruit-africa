c91ccf3d5a404d14abd8608aca95fdee889f3e09
Luigi Morel <morelmiles@gmail.com>
2021-11-14 04:07:56 +0300

    Make responsive on mobile

--- src/components/AvailableJobs.jsx
+++ src/components/AvailableJobs.jsx
@@ -7,16 +7,16 @@ import { ReactComponent as LocationIcon } from "./../assets/Location.svg";
 const AvailableJobs = () => {
   return (
     <div className="mt-1 bg-yellow-100 flex flex-col items-center mb-14 justify-center">
-      <h3 className="font-bold text-4xl sm:mb-2.5 sm:pt-32 pt-16 text-center">
+      <h3 className="font-bold text-4xl mb-2.5 sm:pt-32 pt-16 px-3 text-center">
         10,245 accounting jobs listed here
       </h3>
       <p className="mb-10">Your dream job is waiting for you</p>
 
       <form action="">
-        <div className="flex flex-col sm:flex-row  items-center justify-center bg-white w-auto p-2 mb-40">
-          <div className="flex flex-row items-center ">
+        <div className="flex flex-col sm:flex-row  items-center sm:justify-center sm:bg-white p-2 mb-20 sm:mb-40">
+          <div className="flex sm:flex-row flex-col items-center justify-center ">
             {/* Job title */}
-            <label class="relative text-gray-400 mr-3 focus-within:text-gray-600 block">
+            <label class="relative text-gray-400 sm:mr-3 mb-4 focus-within:text-gray-600 block">
               <SearchIcon className="absolute w-6 h-6 top-2.5  left-4" />
 
               <input
@@ -31,7 +31,7 @@ const AvailableJobs = () => {
             </label>
 
             {/* Location */}
-            <label class="relative text-gray-400 focus-within:text-gray-600 block mr-3">
+            <label class="relative text-gray-400 mb-4 focus-within:text-gray-600 block  sm:mr-3">
               <LocationIcon className="absolute w-6 h-6 top-3  left-4" />
               <input
                 type="text"
