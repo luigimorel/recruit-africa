c1b3eb14213d9b145807f937e452bec3911f1cff
Luigi Morel <morelmiles@gmail.com>
2021-11-14 04:09:46 +0300

    Add drop down menu state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 //Assets
 import Logo from "./../assets/Logo.svg";
-import DropdownIcon from "./../assets/Dropdown.svg";
-import { ReactComponent as BurgerMenu } from "./../assets/BurgerMenu.svg";
+import { ReactComponent as DropdownIcon } from "./../assets/Dropdown.svg";
+import { ReactComponent as BurgerMenu } from "./../assets/Menu.svg";
 
 const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   const menuList = [
     {
       id: 1,
@@ -28,7 +30,7 @@ const Navbar = () => {
   return (
     <div className="flex flex-row justify-between items-center py-8  ">
       <Link to="/">
-        <img src={Logo} loading="lazy" alt="" />
+        <img src={Logo} loading="lazy" alt=" " />
       </Link>
 
       <p className="sm:hidden">
@@ -43,12 +45,38 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <Link to="/candidate-signup">
-          <button className="bg-yellow-300 flex flex-row py-4 px-8 items-center">
-            <span className="font-medium"> Signup for free</span>
-            <img loading="lazy" src={DropdownIcon} className="ml-4" alt="" />
-          </button>
-        </Link>
+        <div>
+          <div className="dropdown inline-block relative">
+            <button
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+              className="bg-yellow-500 text-black font-semibold py-4 px-8 rounded inline-flex items-center"
+            >
+              <span className="mr-1">Signup for free</span>
+              <DropdownIcon className="ml-4" />
+            </button>
+
+            {dropdownOpen && (
+              <ul className="dropdown-menu absolute bg-white hidden w-52 pt-2">
+                <li className="">
+                  <NavLink
+                    className="rounded-t bg-white hover:text-yellow-400  py-3 px-8  block whitespace-no-wrap"
+                    to="/candidate-signup"
+                  >
+                    Find a Job
+                  </NavLink>
+                </li>
+                <li className="">
+                  <NavLink
+                    className="bg-white hover:text-yellow-400 py-3 px-8 block whitespace-no-wrap"
+                    to="/recruitor-signup"
+                  >
+                    Post a Job
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
