5ce47945d8c3dcf9d7b9df8f62d6906106cf95c1
Luigi Morel <morelmiles@gmail.com>
2021-11-17 00:33:43 +0300

    Separate components

--- src/pages/accounts/candidates/dashboard/Dashboard.jsx
+++ src/pages/accounts/candidates/dashboard/Dashboard.jsx
@@ -1,40 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 //Assets
-import { ReactComponent as Logo } from "./../../../../assets/Logo.svg";
-import { ReactComponent as Bell } from "./../../../../assets/accounts/Bell.svg";
-import { ReactComponent as Nilov } from "./../../../../assets/accounts/Nilov.svg";
 import { ReactComponent as Location } from "./../../../../assets/Location.svg";
 import JobLogo from "../../../../assets/JobLogo.png";
 import { ReactComponent as Verified } from "./../../../../assets/accounts/Verified.svg";
 import { ReactComponent as Bookmark } from "./../../../../assets/accounts/Bookmark.svg";
 import { ReactComponent as Star } from "./../../../../assets/Star.svg";
 
-const CandidatesDashboard = () => {
-  const navMinor = [
-    {
-      id: 1,
-      text: "Jobs",
-      route: "/",
-    },
-    {
-      id: 2,
-      text: "Applications",
-      route: "/",
-    },
-    {
-      id: 3,
-      text: "My resume",
-      route: "/",
-    },
-    {
-      id: 4,
-      text: "My account",
-      route: "/",
-    },
-  ];
+//Components
+import DashboardSidebar from "./components/sidebar.dashboard";
+import DashboardNavMinor from "./components/navMinor.dashboard";
 
+const CandidatesDashboard = () => {
   const preferences = [
     {
       id: 1,
@@ -67,6 +45,7 @@ const CandidatesDashboard = () => {
       experience: "3 to 5 years",
       type: "Full time",
       salary: "2,000 & Above",
+      time: 3,
     },
     {
       id: 2,
@@ -76,77 +55,16 @@ const CandidatesDashboard = () => {
       experience: "3 to 5 years",
       type: "Full time",
       salary: "2,000 & Above",
+      time: 3,
     },
   ];
 
   return (
     <div className="flex flex-row pt-12 ">
-      <div className="ml-12">
-        <Logo className=" mb-20" />
-        <Nilov className="mb-7" />
-
-        <div className="mb-12">
-          <h3 className="text-gray-800 font-bold text-lg mb-2.5">
-            John Ebuka Musa
-          </h3>
-          <span className="text-gray-800 text-sm">Payable accountant</span>
-        </div>
-        <div className="flex flex-col mb-12">
-          <div className="flex flex-row justify-between">
-            <h6 className="mb-2.5 text-gray-800 font-bold">Job preference</h6>
-            <span className=" text-yellow-500 cursor-pointer hover:text-yellow-400">
-              Edit
-            </span>
-          </div>
-
-          {/* Job choices */}
-          {preferences.map((x) => (
-            <div key={x.id} className="flex flex-row justify-between">
-              <h6 className="mb-2.5 text-gray-800">{x.text}</h6>
-              <span className="text-gray-800 ">{x.choice}</span>
-            </div>
-          ))}
-        </div>
-        <div className="flex flex-col mb-12">
-          <div className="flex flex-row justify-between">
-            <h6 className="mb-2.5 text-gray-800 font-bold">
-              Salary Preference
-            </h6>
-            <span className=" text-yellow-500 cursor-pointer hover:text-yellow-400">
-              Edit
-            </span>
-          </div>
-
-          <div className="flex flex-row justify-between">
-            <h6 className="mb-2.5 text-gray-800">UGX 2,000,000 - 3,000,000</h6>
-          </div>
-        </div>
-        <div className="flex flex-col mb-12">
-          <div className="flex flex-row justify-between">
-            <h6 className="mb-2.5 text-gray-800 font-bold">Location</h6>
-            <span className=" text-yellow-500 cursor-pointer hover:text-yellow-400">
-              Edit
-            </span>
-          </div>
-
-          <div className="flex flex-row justify-between">
-            <h6 className="mb-2.5 text-gray-800">Entire Uganda</h6>
-          </div>
-        </div>
-      </div>
-
+      <DashboardSidebar />
       {/* Start of the navbar */}
       <div className="ml-28 mt-1 mr-28 w-3/5 ">
-        <div className="flex flex-row mb-16">
-          <ul className="flex flex-row">
-            {navMinor.map((x) => (
-              <li className="mr-10 minor-nav text-gray-600" key={x.id}>
-                <NavLink to={x.route}>{x.text}</NavLink>
-              </li>
-            ))}
-          </ul>
-          <Bell className="ml-8" />
-        </div>
+        <DashboardNavMinor />
 
         <form action="" method="post" className="mb-16 ">
           <h6 className="font-bold mb-4">Search for jobs</h6>
@@ -173,55 +91,59 @@ const CandidatesDashboard = () => {
 
         <div className="flex flex-col">
           {jobs.map((x) => (
-            <div className="mb-10 shadow-lg  pt-8 flex flex-col ">
-              <div className="flex flex-col mx-10">
-                <div className="flex flex-row items-center mb-7">
-                  <img src={x.img} width="60" alt="" />
-                  <div className="flex flex-col ml-5">
-                    <h6 className="font-bold text-xl text-gray-800">
-                      {x.title}
-                    </h6>
+            <Link to={"/candidate-dashboard/" + x.id}>
+              <div className="mb-10 shadow-lg  pt-8 flex flex-col ">
+                <div className="flex flex-col mx-10">
+                  <div className="flex flex-row items-center mb-7">
+                    <img src={x.img} width="60" alt="" />
+                    <div className="flex flex-col ml-5">
+                      <h6 className="font-bold text-xl text-gray-800">
+                        {x.title}
+                      </h6>
 
-                    <span className="flex flex-row items-center ">
-                      <Location className="mr-2" />
-                      <span className="text-sm text-gray-400">
-                        {x.location}
+                      <span className="flex flex-row items-center ">
+                        <Location className="mr-2" />
+                        <span className="text-sm text-gray-400">
+                          {x.location}
+                        </span>
                       </span>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-3 mb-7">
+                    <span>
+                      <h3 className="text-gray-500 text-xs">Experience</h3>
+                      <p className="font-bold text-gray-700">{x.experience}</p>
+                    </span>
+                    <span>
+                      <h3 className="text-gray-500 text-xs">Job type</h3>
+                      <p className="font-bold text-gray-700">{x.type}</p>
+                    </span>
+                    <span>
+                      <h3 className="text-gray-500 text-xs">Salary</h3>
+                      <p className="font-bold text-gray-700">$ {x.salary}</p>
                     </span>
                   </div>
                 </div>
-
-                <div className="grid grid-cols-3 mb-7">
-                  <span>
-                    <h3 className="text-gray-500 text-xs">Experience</h3>
-                    <p className="font-bold text-gray-700">{x.experience}</p>
-                  </span>
-                  <span>
-                    <h3 className="text-gray-500 text-xs">Job type</h3>
-                    <p className="font-bold text-gray-700">{x.type}</p>
-                  </span>
-                  <span>
-                    <h3 className="text-gray-500 text-xs">Salary</h3>
-                    <p className="font-bold text-gray-700">$ {x.salary}</p>
-                  </span>
-                </div>
-              </div>
-              <div className=" bg-yellow-50">
-                <div className=" grid grid-cols-3 items-center px-10 py-4">
-                  <span className="flex flex-row items-center">
-                    <Verified className="mr-2.5" />
-                    <p className=" text-xs font-bold  text-green-500">
-                      Verified company
+                <div className=" bg-yellow-50">
+                  <div className=" grid grid-cols-3 items-center px-10 py-4">
+                    <span className="flex flex-row items-center">
+                      <Verified className="mr-2.5" />
+                      <p className=" text-xs font-bold  text-green-500">
+                        Verified company
+                      </p>
+                    </span>
+                    <p className="text-xs text-gray-500">
+                      Posted {x.time} mins ago
                     </p>
-                  </span>
-                  <p className="text-xs text-gray-500">Posted 3 mins ago</p>
-                  <button className="flex flex-row items-center">
-                    <Bookmark className="mr-2.5" />
-                    <p className=" text-xs text-gray-500">Save Job</p>
-                  </button>
+                    <button className="flex flex-row items-center">
+                      <Bookmark className="mr-2.5" />
+                      <p className=" text-xs text-gray-500">Save Job</p>
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
