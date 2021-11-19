3955e5cd05bfa4d4b3bf76b75147ebd31af6851a
Luigi Morel <morelmiles@gmail.com>
2021-11-19 03:04:31 +0300

    Update the custom checkbox

--- src/common/Checkbox.jsx
+++ src/common/Checkbox.jsx
@@ -1,12 +1,12 @@
 export default function Checkbox(props) {
-  const { className, label, onChange } = props;
+  const { className, label, onChange, number } = props;
 
   return (
     <div className={className}>
       <label className="cursor-pointer w-full">
         <input type="checkbox" className="hidden peer" onChange={onChange} />
-        <span className="text-[#6E6E6E] peer-checked:p-3 peer-checked:mx-1 font-medium peer-checked:bg-white   peer-checked:text-[#FA6E59]">
-          {label}
+        <span className="block px-4 sm:px-8 py-4 mr-2 peer-checked:bg-yellow-400 font-medium  peer-checked:text-gray-800">
+          {label} ({number})
         </span>
       </label>
     </div>
